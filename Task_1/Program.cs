2184ee42ce435f07b76f439e32ac1335c241f04f
Sergey <spumen35@gmail.com>
2023-12-07 20:55:09 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+ 
+// проверка кратности чисел 7 и 23
+
+Console.Write("Введите число number: ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 7 == 0 || x % 23 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
