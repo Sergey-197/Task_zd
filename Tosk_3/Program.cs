e113cdc614014601022fb8fc7686dcfd9dd0f44a
Sergey <spumen35@gmail.com>
2023-12-07 20:56:53 +0300

    Tosk_3

---
+++ Tosk_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+ //Напишите программу, которая принимает на вход целое
+  //число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+ Console.WriteLine("Введите число из отреза [10, 99]:");
+ 
+  int number = Convert.ToInt32(Console.ReadLine());
+ 
+  int digit1 = number / 10;
+ 
+  int digit2 = number % 10;
+ 
+  int maxdigit = digit1 > digit2 ? digit1 : digit2;
+ 
+  Console.WriteLine(maxdigit);
+
+  
+
+
+
