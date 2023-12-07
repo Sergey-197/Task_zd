a18d40ffae271914fd5671bd47f5dbffd8423bf5
Sergey <spumen35@gmail.com>
2023-12-07 20:57:33 +0300

    Tosk_4

---
+++ Tosk_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+ //Напишите программу, которая на вход принимает натуральное число N, а 
+ // на выходе показывает его цифры через запятую
+
+Console.Write("Введите натуральное число N: ");
+ 
+int num = int.Parse(Console.ReadLine());
+
+int even = 0;
+
+while (num >= even) // even -возвращает четное целое число
+{
+    Console.Write(even + ",");
+     
+     even = even + 1;
+}
+
+ 
